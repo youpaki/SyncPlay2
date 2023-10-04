24d17baa00051a7ee5eb6b8597e356504727c19b
youpaki <youpakikillllller@gmail.com>
2023-10-05 00:34:48 +0200

    faut changer get isrc sinon added

--- maine34.py
+++ maine34.py
@@ -36,7 +36,7 @@ spotify_playlist_track_name = []
 youtube_playlist_isrc: []
 youtube_playlist_artist: []
 youtube_playlist_track_name: []
-
+diffs = []
 
 separator = '(feat'
 
@@ -47,41 +47,32 @@ def get_current_spotify():    #get current spotify
     spoplaylist = sp.playlist_items(plSPO_id)
     spoitemplaylist = spoplaylist["items"]
 
-
-    for items in spoitemplaylist:
-        l = l+1
-
-    print(l)
-
-
-
-
     for items in spoitemplaylist:
         spotify_playlist_isrc.append(items['track']['external_ids']['isrc'])
         spotify_playlist_track_name.append(items['track']['name'])
         spotify_playlist_artist.append(items['track']['artists'][0]['name'])
 
+    return(spotify_playlist_track_name,spotify_playlist_artist,spotify_playlist_isrc)
 
 
-    def comptespodbug():
-        #DEBUG recomptage de toutes les listes
-        k=0
-        for items in spotify_playlist_isrc:
-            k = k+1
-        if k != l:
-            print('ola ya un soucis isrcs')
-        k = 0
-
-        for items in spotify_playlist_track_name:
-            k = k+1
-        if k != l:
-            print('ola ya un soucistrckn ame')
-        k = 0
-        for items in spotify_playlist_artist:
-            k = k+1
-        if k != l:
-            print('ola ya un soucis sp artist')
-        k = 0
+def comptespodbug():
+    #DEBUG recomptage de toutes les listes
+    k=0
+    for items in spotify_playlist_isrc:
+        k = k+1
+    if k != l:
+        print('ola ya un soucis isrcs')
+    k = 0
+    for items in spotify_playlist_track_name:
+        k = k+1
+    if k != l:
+        print('ola ya un soucistrckn ame')
+    k = 0
+    for items in spotify_playlist_artist:
+        k = k+1
+    if k != l:
+        print('ola ya un soucis sp artist')
+    k = 0
 
 
 
@@ -98,7 +89,7 @@ def get_current_ytb():
         youtube_playlist_artist.append(items['artists'][0]['name'])
         k = k+1
         youtube_playlist_isrc.append(get_isrc_(items['title'],items['artists'][0]['name']))
-    return(youtube_playlist_track_name,yout)
+    return(youtube_playlist_track_name,youtube_playlist_artist,youtube_playlist_isrc)
 
 
 
@@ -110,11 +101,70 @@ def get_current_ytb():
 def get_isrc_(track_name,artist):
     artist = artist.replace(' ', '+')
     name = track_name.replace(' ', '+')
-    spoisrcquery = name + '+' + artist + '&type=track&limit=1'
-    search_results = sp.search(q=spoisrcquery)
+    spoisrcquery = name + "%20artist:" +  artist
+    print(spoisrcquery)
+    search_results = sp.search(q=spoisrcquery,limit=1,type="track")
+    print(search_results)
     if search_results["tracks"]["items"] == []:
         print('Nothing found for ' + track_name + artist)
     else:
-        return(search_results['tracks']['items'][0]['external_ids'])
+        return(search_results['tracks']['items'][0]['external_ids']['isrc'])
+
+def convert_isrc_id(isrc):
+    query = isrc + '&type=track&limit=1'
+    id = sp.search(q=query)['tracks']['items'][0]['id']
+    return(id)
 
 
+def compare_isrc(youtube_isrc,spotify_isrc):
+    if spotify_isrc == youtube_isrc :
+        return(0)
+    else:
+        return(youtube_isrc,spotify_isrc)
+
+
+def compare (ytb_input,spo_input):
+    k=0
+    diffs = []
+    add_ytb = []
+    add_spo = []
+    for items in ytb_input:
+        diffs.append(compare_isrc(ytb_input[k],spo_input[k]))
+        k=k+1
+    for items in diffs:
+        if check_ytb(items,ytb_input) == False:
+            add_ytb.append(items)
+        else:
+            print("boah")
+        if check_spo(items,spo_input) == False:
+            add_spo.append(items)
+        else:
+            print("boba")
+    return(add_ytb,add_spo)
+
+
+
+def check_ytb(isrc,ytb):
+    if isrc in ytb:
+        return(True)
+    else:
+        return(False)
+def check_spo(isrc,spo):
+    if isrc in spo:
+        return(True)
+    else:
+        return(False)
+
+
+def add_spo(track,artist,isrc):
+    sp.search()
+
+
+print(get_isrc_("Doja","Central Cee"))
+#print(convert_isrc_id())
+
+
+#print(get_current_ytb())
+#print(get_current_spotify())
+
+#print(compare(get_current_spotify(),get_current_ytb()))
